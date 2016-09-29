588e6b4bc05f990364d87d00efb2b596b5b3624d
DanMurphy15 <danieljmurphy01@gmail.com>
2016-09-29 11:52:18 -0400

    Update Alchohol_Consumption.r

--- Data-Set-2/Alchohol_Consumption.r
+++ Data-Set-2/Alchohol_Consumption.r
@@ -17,3 +17,13 @@ hist(beer_servings)
 hist(spirit_servings)
 
 cor(Alchohol_Raw[-1], method = "pearson")
+
+top6 <- head(Alchohol_Raw)
+barplot(top6$beer_servings, main = "Beer Servings", xlab = "Country", ylab = "Liters",
+        names.arg = c("Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua & Barbuda"), 
+        border = "black", col = "blue")
+
+plot(beer_servings, total_litres_of_pure_alcohol)
+abline(lm(total_litres_of_pure_alcohol~beer_servings))
+text(beer_servings, total_litres_of_pure_alcohol, labels = country, cex = .5, pos = 4, col = "green")
+
